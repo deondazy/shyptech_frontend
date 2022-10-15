16a41ef57f9f4f9cde2cf744f7a5c4ccdbb5838f
quervolvh <quervolvh@gmil.com>
2022-10-15 15:46:23 +0100

    youth culture

--- src/pages/track/index.tsx
+++ src/pages/track/index.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { LandingLayout } from 'layout';
-import { shipmentProcess } from 'redux/actions/ShipmentAction';
-import { useDispatch } from 'react-redux';
 import { providerFormType } from 'types';
-import { TabLayout } from 'components';
 import { useRouter } from 'next/router';
-import { isObj } from 'utils';
-import { getAddressInfo } from 'redux/actions/UtilActions';
 import { TrackDeliveryForm } from 'common/Track/TrackDeliveryForm';
 import { SectionOne } from 'common/Landing/SectionOne';
 
 const Track: React.FC<Props> = ({ isMobile, deviceWidth }) => {
 
-    const dispatch = useDispatch();
-
     const router = useRouter();
 
-    const { view, reference } = router.query;
+    // const { view, reference } = router.query;
 
-    const [state, setState] = useState<{
+    const [, setState] = useState<{
         beep: number,
         data?: providerFormType
     }>({
@@ -32,15 +25,6 @@ const Track: React.FC<Props> = ({ isMobile, deviceWidth }) => {
         router.push("track?view=tracking")
 
     };
-
-    useEffect(() => {
-
-        dispatch(shipmentProcess("get-providers", { service: "1" }));
-
-        dispatch(getAddressInfo("retrieve-countries", {}));
-
-        // eslint-disable-next-line
-    }, []);
 
     return (
 
